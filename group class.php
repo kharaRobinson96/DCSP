440f2a490bec9a8f23da415620239605003b2174
kharaRobinson96 <43385947+kharaRobinson96@users.noreply.github.com>
2018-11-14 23:46:29 -0600

    Add files via upload

--- group class.php
+++ group class.php
@@ -1,32 +1,25 @@
 <?php
+include("db_functions.php");
 class Group{
-    public $groupID, $groupName;
-    function __construct($groupID)
+    private $groupID ="" ;
+    private $groupName= "";
+    public function Group(int $groupID)
     {
-        //Constructor. Set groupID. Retrieve groupName from database and store.
-        $this->groupID=$groupID;
+    //Constructor. Set groupID. Retrieve groupName from database and store.
+    $setGroupID= new Group();
 
-
+    $setGroupID->groupID=$groupID;
     }
-
-    function __construct($groupName)
+    public function Group(string $groupName)
     {
-        //Constructor. Set groupName, create new group and store in database.
-
-        $this->groupName=$groupName;
-
+    //Constructor. Set groupName, create new group and store in database.
+    $newGroupName= new Group();
+    $newGroupName->groupName=$groupName;
     }
-
     public function getUsers()
     {
-        //Returns a list of userIDs for users in the group.
-        return array("SELECT *
-                      FROM Users
-                      WHERE userID= $userID");
-        
-
-        
+    //Returns a list of userIDs for users in the group.
+    return get_users_from_group($this->$groupID);
     }
-
 }
-?>
+?>`
--- poll class.php
+++ poll class.php
@@ -1,93 +1,58 @@
 <?php
-class Poll{
-    public $userID, $pollID, $groupID, $pollOptionID, $count, $voteCount;
+include("db_functions.php");
+class Poll {
+    private $pollID="";
+    private $groupID="";
+
     function __construct($pollID)
     {
-        $this->db = $dbConnection;
-        
         //Constructor. Retrieve existing poll from database.
-      /* $this->$userID = $userID;
-       $this->$pollID = $pollID;
-       $this->$groupID = $groupID;
-       $this->$pollOptionID = $pollOptionID;
-*/
-        $result = mysqli_query($this->dbConnect, $sqlQuery);
-        if(!$result){
-            die('Error in query: '. mysqli_error());
-        }
-        $data= array();
-        while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) {
-            $data[]=$row;
-        }
-        return $data;
-        }
+        $this->pollID=$pollID;
+
     }
     function __construct($groupID)
     {
-        if(!$this->db){ 
-            // Connect to the database
-            $conn = new mysqli($this->dbHost, $this->dbUser, $this->dbPwd, $this->dbName);
-            if($conn->connect_error){
-                die("Failed to connect with MySQL: " . $conn->connect_error);
-            }else{
-                $this->db = $conn;
-            }
-        }
+       //Constructor. Create new poll and store in database.
+       $newPoll->groupID=$groupID;
+
     }
     public function isOpen()
     {
         //Returns true if the poll is open, false if it is closed.
-        if(isOpen()= 1)
-        {
-            return true;
+        if($this->pollID == false){
+            $this->pollID= set_poll_closed($groupID);
         }
-        if(isOpen()= 0)
-        {
-            return false;
-        }
+        return $this->pollID;
 
     }
     public function setOpen()
     {
         //Sets poll to open, stores in database.
-        $this->$poll(true);
+        set_poll_open($this->groupID);
 
     }
     public function setClosed()
     {
+
         //Sets poll to closed, stores in database.
-        $this->$poll(false);
+        set_poll_closed($this->groupID);
     }
     public function getResults()
     {
-    
+    //Gets results of poll.
+    get_details_for_option($this->pollOptionID);
     }
     public function vote($userID, $pollOptionID)
     {
         //Creates a vote for a poll option by specified user and stores in database. 
-        {
-            if(!isset($data['pollId']) || !isset($data['pollOptionId']) || isset($_COOKIE[$data['pollId']])) {
-                return false;
-            }else{
-                $sql = "SELECT * FROM ".$this->Restaurant." WHERE pollId = ".$data['pollId']." AND pollOptionId = ".$data['pollOptionId'];
-                $preVote = $this->getQuery($sql, 'count');
-                if($preVote > 0){
-                    $query = "UPDATE ".$this->Restaurant." SET voteCount = voteCount+1 WHERE pollId = ".$data['pollId']." AND pollOptionId = ".$data['pollOptionId'];
-                    $update = $this->db->query($query);
-                }else{
-                    $query = "INSERT INTO ".$this->Restaurant." (pollId,pollOptionId,voteCount) VALUES (".$data['pollId'].",".$data['pollOptionId'].",1)";
-                    $insert = $this->db->query($query);
-                }
-                return true;
-            }
+        cast_vote($this->pollOptionID, $userID);
     }
     public function usersVotedList() 
     {
         //Retrieves which users have voted.
-        "SELECT * FROM Restaurant WHERE $userID AND $pollOptionID";
-
-
+        get_votes_for_option($this->pollOptionID);
     }
+    
 }
 
 
