c7f83fcfdcad8ce0f4b4ea9db68c33bc1386afd4
kharaRobinson96 <43385947+kharaRobinson96@users.noreply.github.com>
2018-11-19 18:33:57 -0600

    Update group class.php

--- group class.php
+++ group class.php
@@ -1,21 +1,50 @@
 <?php
 include("db_functions.php");
 class Group{
-    private $groupID ="" ;
-    private $groupName= "";
-    public function Group(int $groupID)
-    {
-    //Constructor. Set groupID. Retrieve groupName from database and store.
-    $setGroupID= new Group();
+    private $objectID = 0; //any negative number will work
+	private $objectName = "";
+	private $attribute1 ="";
+		
+	
+		//if this were a class for a ball, name this function createNewBall
+		public static function createNewObject($arg1, $arg2, . . . $argN) {
+			//note: do NOT pass the classID as an argument
+			//to make a new object of this class, call this in your code (this is for frontend):
+			// $object = ClassName::createNewObject();
 
-    $setGroupID->groupID=$groupID;
-    }
-    public function Group(string $groupName)
-    {
-    //Constructor. Set groupName, create new group and store in database.
-    $newGroupName= new Group();
-    $newGroupName->groupName=$groupName;
-    }
+			// make an empty object
+			$newObject = new ClassName();
+
+			$newObject->attribute1 = arg1;
+			$newObject->attribute1 = arg1;
+			. . .
+			$attributeN->argN;
+
+			// there should be a database function similarly named
+			$newObject-> classID = create_object(ATTRIBUTES)
+
+			return $newObject;
+		}
+
+		public static function retrieveGroup($groupID) {
+			//note: you MUST pass the classID as THE ONLY argument
+			//to make a new object of this class, call this in your code (this is for frontend):
+			// $object = ClassName::retrieveObject();
+
+			// make an empty object
+			$Object = new ClassName();
+
+			$newObject->objectID = $objectID;
+			
+			// there should be a database function similarly named
+			$details = get_object_details_from_id($objectID);
+
+			$newObject->attribute1 = $details["attribute1"];
+			
+
+			return $newObject;
+		}
+    
     public function getUsers()
     {
     //Returns a list of userIDs for users in the group.
