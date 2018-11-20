abf6335404ee8e07f89eee1db853c112668f36ef
kharaRobinson96 <43385947+kharaRobinson96@users.noreply.github.com>
2018-11-20 00:17:31 -0600

    Update poll class.php

--- poll class.php
+++ poll class.php
@@ -3,7 +3,50 @@ include("db_functions.php");
 class Poll {
     private $pollID="";
     private $groupID="";
+    
+    //if this were a class for a ball, name this function createNewBall
+		public static function createNewObject($arg1, $arg2, . . . $argN) {
+			//note: do NOT pass the classID as an argument
+			//to make a new object of this class, call this in your code (this is for frontend):
+			// $object = ClassName::createNewObject();
 
+			// make an empty object
+			$newObject = new ClassName();
+
+			$newObject->attribute1 = arg1;
+			$newObject->attribute1 = arg1;
+			. . .
+			$attributeN->argN;
+
+			// there should be a database function similarly named
+			$newObject-> classID = create_object(ATTRIBUTES)
+
+			return $newObject;
+		}
+
+		public static function retrieveObject($objectID) {
+			//note: you MUST pass the classID as THE ONLY argument
+			//to make a new object of this class, call this in your code (this is for frontend):
+			// $object = ClassName::retrieveObject();
+
+			// make an empty object
+			$Object = new ClassName();
+
+			$newObject->objectID = $objectID;
+			
+			// there should be a database function similarly named
+			$details = get_object_details_from_id($objectID);
+
+			$newObject->attribute1 = $details["attribute1"];
+			$newObject->attribute1 = $details["attribute1"];
+			$newObject->attribute1 = $details["attribute1"];
+			. . .
+			$newObject->attributeN = $details["attributeN"];
+
+			return $newObject;
+		}
+
+/*
     function __construct($pollID)
     {
         //Constructor. Retrieve existing poll from database.
@@ -16,6 +59,7 @@ class Poll {
        $newPoll->groupID=$groupID;
 
     }
+   */
     public function isOpen()
     {
         //Returns true if the poll is open, false if it is closed.
