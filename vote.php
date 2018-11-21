f47119be0473c2cd1a9c5de148a7c4d4fae4d6d0
kharaRobinson96 <43385947+kharaRobinson96@users.noreply.github.com>
2018-11-20 20:59:55 -0600

    Create vote.php

---
+++ vote.php
@@ -0,0 +1,44 @@
+<?php
+include("db_functions.php");
+class Vote {
+    private $userID;
+    private $pollOptionID;
+    private $attribute1 = "";
+    
+    //if this were a class for a ball, name this function createNewBall
+		public static function createNewObject($arg1) {
+			//note: do NOT pass the classID as an argument
+			//to make a new object of this class, call this in your code (this is for frontend):
+			// $object = ClassName::createNewObject();
+
+			// make an empty object
+			$newObject = new ClassName();
+
+			$newObject->attribute1 = arg1;
+			
+
+			// there should be a database function similarly named
+			$newObject-> classID = create_object(ATTRIBUTES)
+
+			return $newObject;
+		}
+
+		public static function retrieveObject($userID) {
+			//note: you MUST pass the classID as THE ONLY argument
+			//to make a new object of this class, call this in your code (this is for frontend):
+			// $object = ClassName::retrieveObject();
+
+			// make an empty object
+			$Object = new ClassName();
+
+			$newObject->userID = $userID;
+			
+			// there should be a database function similarly named
+			$details = get_object_details_from_id($userID);
+
+			$newObject->attribute1 = $details["attribute1"];
+			
+			return $newObject;
+		}
+    public function
+?>
