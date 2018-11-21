6fe595a170d1970e82c274e66e20569ac7c33357
kharaRobinson96 <43385947+kharaRobinson96@users.noreply.github.com>
2018-11-21 17:56:43 -0600

    Update poll class.php

--- poll class.php
+++ poll class.php
@@ -5,86 +5,56 @@ class Poll {
     private $groupID="";
     
     //if this were a class for a ball, name this function createNewBall
-		public static function createNewObject($arg1, $arg2, . . . $argN) {
+		public static function createNewObject() {
 			//note: do NOT pass the classID as an argument
 			//to make a new object of this class, call this in your code (this is for frontend):
 			// $object = ClassName::createNewObject();
-
 			// make an empty object
-			$newObject = new ClassName();
-
-			$newObject->attribute1 = arg1;
-			$newObject->attribute1 = arg1;
-			. . .
-			$attributeN->argN;
-
+			$newObject = new Poll();
+			
 			// there should be a database function similarly named
-			$newObject-> classID = create_object(ATTRIBUTES)
-
+			$newObject-> pollID = create_object()
 			return $newObject;
 		}
-
 		public static function retrieveObject($objectID) {
 			//note: you MUST pass the classID as THE ONLY argument
 			//to make a new object of this class, call this in your code (this is for frontend):
 			// $object = ClassName::retrieveObject();
-
 			// make an empty object
-			$Object = new ClassName();
-
-			$newObject->objectID = $objectID;
+			$Object = new Poll();
+			$newObject->pollID = $pollID;
 			
 			// there should be a database function similarly named
-			$details = get_object_details_from_id($objectID);
-
-			$newObject->attribute1 = $details["attribute1"];
-			$newObject->attribute1 = $details["attribute1"];
-			$newObject->attribute1 = $details["attribute1"];
-			. . .
-			$newObject->attributeN = $details["attributeN"];
-
+			$details = get_object_details_from_id($pollID);
+			$newObject->groupID = $details["groupID"];
 			return $newObject;
 		}
 
-/*
-    function __construct($pollID)
-    {
-        //Constructor. Retrieve existing poll from database.
-        $this->pollID=$pollID;
-
-    }
-    function __construct($groupID)
-    {
-       //Constructor. Create new poll and store in database.
-       $newPoll->groupID=$groupID;
-
-    }
-   */
     public function isOpen()
     {
         //Returns true if the poll is open, false if it is closed.
-        if($this->pollID == false){
-            $this->pollID= set_poll_closed($groupID);
+        if($this->poll()==0 ){
+            return false;
         }
-        return $this->pollID;
+        if($this->poll()==1){
+            return true;
+        }
 
     }
     public function setOpen()
     {
         //Sets poll to open, stores in database.
         set_poll_open($this->groupID);
-
     }
     public function setClosed()
     {
-
         //Sets poll to closed, stores in database.
         set_poll_closed($this->groupID);
     }
     public function getResults()
     {
     //Gets results of poll.
-    get_details_for_option($this->pollOptionID);
+    get_votes_for_option($this->pollOptionID);
     }
     public function vote($userID, $pollOptionID)
     {
@@ -95,10 +65,10 @@ class Poll {
     {
         //Retrieves which users have voted.
         get_votes_for_option($this->pollOptionID);
+        return get_array("SELECT  * 
+                        FROM Votes 
+                        WHERE useerID = $userID");
     }
     
 }
-
-
-
 ?>
