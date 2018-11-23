8fa10aa45bd50dee78af56114836f50d4a27ad59
kharaRobinson96 <43385947+kharaRobinson96@users.noreply.github.com>
2018-11-22 23:39:34 -0600

    Update group class.php

--- group class.php
+++ group class.php
@@ -1,45 +1,41 @@
 <?php
 include("db_functions.php");
 class Group{
-    private $grouptID = 0; //any negative number will work
+    private $groupID = -16; 
 	private $groupName = "";
 		
 			public static function createNewGroup($groupID, $groupName) {
 			
 			$newGroup = new Group();
-			$newGroup->groupName = groupName;
-			$newGroup->groupID = groupID;
+			$newGroup->groupName = $groupName;
+			$newGroup->groupID = $groupID;
 			
-			$newGroup-> groupID = create_group($groupID, $groupName);
-
-			return $newGroup;
+			$newGroup->groupID = create_group($groupID, $groupName);
+ 			return $newGroup;
 		}
 		public static function retrieveGroup($groupID) {
 			
 			$Group = new Group();
-
-			$newGroup->groupID = $groupID;
-			$newGroup->groupName=$groupName;
+ 			$newGroup->groupID = $groupID;
 			
 			$details = get_group_details_from_id($groupID);
 			$newGroup->groupName = $details["groupName"];
-			$newGroup->groupID = $details["groupID"];
-
-			return $newGroup;
+ 			return $newGroup;
 		}
     
     public function getUsers()
     {
     //Returns a list of userIDs for users in the group.
-    return get_users_from_group($this->groupID);
+	return get_users_from_group($this->groupID);
 	}
+	 
+	public function getPolls() {
+		return get_polls_for_group($this->groupID);
+	}
+	
 
-	public function joinGroup($groupID){
-		add_user_to_group($this->userID,$groupID);
+	 public function name() {
+		return $this->groupName;
 	}
-	public function leaveGroup($groupID){
-		remove_user_from_group($this->userID,$groupID);
-
-	}
-}
-?>`
+ }
+?>
