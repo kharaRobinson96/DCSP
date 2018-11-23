709b8790114b657eb86b2c4792a65897434910c1
kharaRobinson96 <43385947+kharaRobinson96@users.noreply.github.com>
2018-11-22 23:38:47 -0600

    Update poll class.php

--- poll class.php
+++ poll class.php
@@ -1,13 +1,12 @@
 <?php
 include("db_functions.php");
 class Poll {
-    private $pollID="";
-    private $groupID="";
+    private $pollID= -16;
+    private $groupID= -16;
     
 		public static function createNewPoll($pollID,$groupID) {
 			
             $newPoll = new Poll();
-            $newPoll->pollID=$pollID;
             $newPoll->groupID=$groupID;
 			
 			$newPoll-> pollID = create_poll($pollID,$groupID);
@@ -20,47 +19,47 @@ class Poll {
 			
 			$details = get_poll_details_from_id($pollID);
             $newPoll->groupID = $details["groupID"];
-            $newPoll->pollID = $details["pollID"];
 
-			return $newPoll;
-		}
-
+            return $newPoll;
+        }
+    }
+     //  Returns true if poll is open, false if it is closed
     public function isOpen()
     {
         //Returns true if the poll is open, false if it is closed.
-        if($this->is_poll_open($pollID)==false ){
-            return false;
-        }
-        if($this->is_poll_open($pollID)==true){
-            return true;
-        }
-
+         return is_open($this->pollID);
     }
+     //  Sets poll to open
     public function setOpen()
     {
-        //Sets poll to open, stores in database.
+        //Sets poll to open, stores in database
         set_poll_open($this->groupID);
     }
+    
+    //  Sets poll to closed
     public function setClosed()
     {
         //Sets poll to closed, stores in database.
         set_poll_closed($this->groupID);
     }
+     //  Gets results of poll. returns an associative array, with the structure of
+    //  result["restaurantID"] = 32, result["voteCount"] = 2
     public function getResults()
     {
     //Gets results of poll.
-    get_votes_for_option($this->pollOptionID);
+        return get_poll_results($this->pollID);
     }
+     //  Creates a vote for a poll option by specified user and stores in database. 
     public function vote($userID, $pollOptionID)
     {
         //Creates a vote for a poll option by specified user and stores in database. 
         cast_vote($this->pollOptionID, $userID);
     }
+    
+    //  Retrieves which users have voted.
     public function usersVotedList() 
     {
-        //Retrieves which users have voted.
-        users_who_voted($this->pollID);
+        get_users_voted($this->pollID);
     }
     
 }
-?>
